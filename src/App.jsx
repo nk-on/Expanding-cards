c628ee2a5e8bca82461d6b412bc1dda6be03459c
nk-on <johndoe@example.com>
2024-11-07 20:43:29 +0400

    Feat:expanding card when clicking it

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,40 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
   const images = [
     {
-      id: 1,
+      id: 0,
       src: 'src/assets/Beach.avif',
     },
     {
-      id: 2,
+      id: 1,
       src: 'src/assets/Bridge.avif',
     },
     {
-      id: 3,
+      id: 2,
       src: 'src/assets/Forest.avif',
     },
     {
-      id: 4,
+      id: 3,
       src: 'src/assets/Mountains.avif',
     },
   ];
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <>
       <div className="Container">
         {images.map((image) => {
-          return <Card key={image.id} imageSrc={image.src} />;
+          return <Card key={image.id} imageSrc={image.src} id = {image.id} activeIndex = {activeIndex} setActiveIndex = {setActiveIndex}/>;
         })}
       </div>
     </>
   );
 }
-function Card({ imageSrc }) {
-  return (
-    <div className= 'Card' style={{background:`url(${imageSrc})`}}>
-      
-    </div>
-  );
+function Card({ imageSrc, id,activeIndex,setActiveIndex}) {
+    return (
+      <div className = {`Card ${id === activeIndex ? 'Expanded': ''}`} onClick ={()=> setActiveIndex(id)} style={{background:`url(${imageSrc})`}}>
+        
+      </div>
+    );
 }
 export default App;
